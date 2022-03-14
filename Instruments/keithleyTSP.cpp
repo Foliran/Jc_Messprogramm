eea335fd89aa125d09c873b6d7e939d2087d44f2
Steinhauser <gu0262@partner.kit.edu>
2022-03-14 14:06:49 +0100

    Calling two scripts on SMU now

--- Instruments/keithleyTSP.cpp
+++ Instruments/keithleyTSP.cpp
@@ -54,29 +54,29 @@ void KeithleyTSP::setPulseAndMeasure(double value, double pWidth, double ratio)
         return;
     }
     pWidth = pWidth / 1000.0;
-    ratio = ratio / 1000.0;
-    //qDebug() << "TSP::setPulseAndMeasure";
-    //+std::to_string(value)+
-    gpib->cmd(address,
-        " node[2].smua.trigger.source.listi({"+std::to_string(value)+"}) ",
-        DELAYGPIB, TERMCHAR);
-    gpib->cmd(address, " node[2].trigger.timer[1].delay = " + std::to_string(pWidth) +
-        " node[2].trigger.timer[2].delay = " + std::to_string(ratio) +
-        " node[2].smua.source.output = node[2].smua.OUTPUT_ON "
-
-        " node[1].trigger.model.initiate() "
-        " node[2].smua.trigger.initiate() "
-
-        " waitcomplete() "
-        " node[2].smua.source.output = node[2].smua.OUTPUT_OFF ",
-        DELAYGPIB, TERMCHAR);
-
+    //ratio = ratio / 1000.0;
+    ratio = 13 / 1000.0;
+    gpib->cmd(address, "initializeSettings.run()", DELAYGPIB, TERMCHAR);
+    std::string valueString = " value = " + std::to_string(value);
+    std::string pWidthString = " pWidth = " + std::to_string(pWidth);
+    std::string ratioString = " ratio = " + std::to_string(ratio);
+    gpib->cmd(address, valueString, DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, pWidthString, DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, ratioString, DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "setPulseAndMeasure.run()", DELAYGPIB, TERMCHAR);
+    /*gpib->cmd(address, "node[2].smua.trigger.source.listi({value})", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[2].trigger.timer[1].delay = pWidth", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[2].trigger.timer[2].delay = ratio", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[2].smua.source.output = node[2].smua.OUTPUT_ON", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[1].trigger.model.initiate()", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[2].smua.trigger.initiate()", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "waitcomplete()", DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, "node[2].smua.source.output = node[2].smua.OUTPUT_OFF", DELAYGPIB, TERMCHAR);*/
     current = std::stod(gpib->query(address, " print(node[2].smua.nvbuffer1.sourcevalues[1]) ", DELAYGPIB, TERMCHAR));
     voltage = std::stod(gpib->query(address, " print(node[1].defbuffer1.readings[1]) ", DELAYGPIB, TERMCHAR));
-    gpib->cmd(address,
-        " node[2].smua.nvbuffer1.clear() "
-        " node[1].defbuffer1.clear() ",
-        DELAYGPIB, TERMCHAR);
+    gpib->cmd(address, " node[2].smua.nvbuffer1.clear() "
+                       " node[1].defbuffer1.clear() ", DELAYGPIB, TERMCHAR);
+
 }
 
 double KeithleyTSP::getVoltage()
